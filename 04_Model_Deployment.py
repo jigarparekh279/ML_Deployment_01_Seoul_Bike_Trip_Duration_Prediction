4309e5ab88641518d54b1e2edca1ac23ffe54e50
Jigar Parekh <40745770+jigarparekh279@users.noreply.github.com>
2021-09-17 02:35:03 +0200

    Add files via upload

--- 04_Model_Deployment.py
+++ 04_Model_Deployment.py
@@ -3,7 +3,6 @@ import joblib
 import os
 
 from sklearn.preprocessing import StandardScaler
-import tensorflow as tf
 from tensorflow import keras
 
 import streamlit as st
@@ -35,16 +34,16 @@ with st.form("prediction_form"):
 
     st.header("Enter the Deciding Factors:")
 
-    distance  = st.number_input("Distance: ", value=0, format="%d")
-    haversine = st.number_input("Haversine: ")
-    plong = st.number_input("Pickup Longitude: ")
-    dlatd = st.number_input("Dropoff Latitude: ")
-    phour = st.slider("Pickup Hour: ", 0, 23, value=0, format="%d")
-    pmin  = st.slider("Pickup Minute: ", 0, 59, value=0, format="%d")
-    dhour = st.slider("Dropoff Hour: ", 0, 23, value=0, format="%d")
-    dmin  = st.slider("Dropoff Minute: ", 0, 59, value=0, format="%d")
-    temp  = st.number_input("Temp: ")
-    solar = st.number_input("Solar: ")
+    distance  = st.number_input("Distance: ", value=5990, format="%d")
+    haversine = st.number_input("Haversine: ", value=1.22)
+    plong = st.number_input("Pickup Longitude: ", value=37.55)
+    dlatd = st.number_input("Dropoff Latitude: ", value=126.92)
+    phour = st.slider("Pickup Hour: ", 0, 23, value=13, format="%d")
+    pmin  = st.slider("Pickup Minute: ", 0, 59, value=4, format="%d")
+    dhour = st.slider("Dropoff Hour: ", 0, 23, value=14, format="%d")
+    dmin  = st.slider("Dropoff Minute: ", 0, 59, value=2, format="%d")
+    temp  = st.number_input("Temp: ", value=25.6)
+    solar = st.number_input("Solar: ", value=2.82)
 
     submit_val = st.form_submit_button("Predict Duration")
 
