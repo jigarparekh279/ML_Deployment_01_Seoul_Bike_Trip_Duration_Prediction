c26ad4ac161f5fe8ed31c7b15153938070575ded
Jigar Parekh <40745770+jigarparekh279@users.noreply.github.com>
2021-09-21 17:21:32 +0200

    Add files via upload

--- 04_Model_Deployment.py
+++ 04_Model_Deployment.py
@@ -12,8 +12,8 @@ import streamlit as st
 # Capture the path on current folder on cloud
 curr_path = os.path.dirname(os.path.realpath(__file__))
 
-feat_cols = ['Distance', 'PLong', 'DLatd', 'Haversine', 
-             'Phour', 'Pmin', 'Dhour', 'Dmin', 'Temp', 'Solar']
+feat_cols = ['Distance', 'Haversine', 'Phour', 'Pmin', 'Dhour',
+             'Dmin', 'Temp', 'Humid', 'Solar', 'Dust']
 
 scalar = joblib.load('models/scalar.joblib')
 model = keras.models.load_model('models/ANN.h5')
@@ -34,23 +34,24 @@ with st.form("prediction_form"):
 
     st.header("Enter the Deciding Factors:")
 
-    distance  = st.number_input("Distance: ", value=5990, format="%d")
-    haversine = st.number_input("Haversine: ", value=1.22)
-    plong = st.number_input("Pickup Longitude: ", value=37.55)
-    dlatd = st.number_input("Dropoff Latitude: ", value=126.92)
-    phour = st.slider("Pickup Hour: ", 0, 23, value=13, format="%d")
-    pmin  = st.slider("Pickup Minute: ", 0, 59, value=4, format="%d")
-    dhour = st.slider("Dropoff Hour: ", 0, 23, value=14, format="%d")
-    dmin  = st.slider("Dropoff Minute: ", 0, 59, value=2, format="%d")
-    temp  = st.number_input("Temp: ", value=25.6)
-    solar = st.number_input("Solar: ", value=2.82)
+    distance  = st.number_input("Distance: ", value=8490, format="%d")
+    haversine = st.number_input("Haversine: ", value=3.400058)
+    phour = st.slider("Pickup Hour: ", 0, 23, value=19, format="%d")
+    pmin  = st.slider("Pickup Minute: ", 0, 59, value=14, format="%d")
+    dhour = st.slider("Dropoff Hour: ", 0, 23, value=20, format="%d")
+    dmin  = st.slider("Dropoff Minute: ", 0, 59, value=12, format="%d")
+    temp  = st.number_input("Temp: ", value=8.8)
+    humid  = st.number_input("Humid: ", value= 49.0)
+    solar = st.number_input("Solar: ", value=0.05)
+    dust  = st.number_input("Dust: ", value=27.0)
 
     submit_val = st.form_submit_button("Predict Duration")
 
+    
 if submit_val:
     # If submit is pressed == True
-    attributes = np.array([distance, plong, dlatd, haversine, phour,
-                        pmin, dhour, dmin, temp, solar]).reshape(1,-1)
+    attributes = np.array([distance, haversine, phour, pmin, dhour,
+                           dmin, temp, humid, solar, dust]).reshape(1,-1)
 
     if attributes.shape == (1,10):
         print("Attributes are valid")
